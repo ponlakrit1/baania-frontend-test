1ddc2d26aa391d38b04fd0ade8614a06060e334f
ponlakrit1 <ponlakritkm@gmail.com>
2023-05-30 10:51:25 +0700

    ADD - add angular bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HouseComponent } from './components/house/house.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/house', pathMatch: 'full' },
+  { path: 'house', component: HouseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
